96ce7f9bd462797aeaf0618d01c8d4859b66f533
Roman <kaboom.ua@gmail.com>
2017-02-15 22:45:36 +0200

    Update DateRange.js

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -4,7 +4,7 @@ import DayPicker, { DateUtils } from 'react-day-picker'
 import 'react-day-picker/lib/style.css';
 
 class DateRange extends Component {
-
+    //почему не вынес значения в стор?
     state = {
         from: null,
         to: null
