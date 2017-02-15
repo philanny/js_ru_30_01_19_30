f4f8301da37104c23e8c1cd48cdb9d023748db8a
Roman <kaboom.ua@gmail.com>
2017-02-15 22:44:36 +0200

    Update articles.js

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -8,6 +8,8 @@ export default (articles = defaultArticles, action) => {
         case DELETE_ARTICLE:
             return articles.filter(article => article.id !== payload.id)
         case SELECT_ARTICLE:
+            //вспомни, что редюсеры - чистые функции, нельзя брать внешние переменные
+            //по сути ты удаляешь статьи, а должен хранить здесь список всех статей, а фильтровать где-то в другом месте(например в коннекте)
             return defaultArticles.filter(article => payload.selection.map( elem => elem.value ).indexOf(article.id) !== -1)
         case SELECT_ARTICLE_BY_DATE:
             return defaultArticles.filter(article => payload.date == article.date)
